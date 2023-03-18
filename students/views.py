30da068465d549ddeffa3accdf21292147f2b357
Cotofrec Victor <cotofrecvictor@gmail.com>
2023-03-18 15:58:02 +0200

    added view student button

--- students/views.py
+++ students/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 from .models import Student
 
@@ -7,3 +9,7 @@ def index(request):
     return render(request,'students/index.html', {
         'students': Student.objects.all()
  })
+
+def view_student(request, id):
+    student = Student.objects.get(pk=id)
+    return HttpResponseRedirect(reverse('index'))
